4bb9d65165419a8339f6fb7be2f925e8c73f4b39
Palesapp <nnupalesa.pp@gmail.com>
2024-04-04 03:10:06 -0700

    update

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,39 +1,33 @@
 #!/usr/bin/python3
-""" Fabfile to distribute an archive to a web server """
+"""Distributes an archive to your web servers, using the function do_deploy"""
+from fabric.contrib import files
+from fabric.api import env, put, run
+import os
 
-from fabric.api import *       # we need to import put, run, env
-from datetime import datetime
-from os.path import exists
-
-env.hosts = ['100.26.49.247', '54.173.27.192']
+env.hosts = ['54.157.182.28', '54.209.136.185']
 
 
 def do_deploy(archive_path):
-    """ distributes an archive to the web servers """
+    """
+    Creates a tar gzipped archive from the web_static directory
+    """
+    if not os.path.exists(archive_path):
+        return False
 
-    if exists(archive_path) is False:
-        return False  # Returns False if the file at archive_path doesnt exist
-    filename = archive_path.split('/')[-1]
-    # filename is <web_static_20230503193956.tgz>
-    no_tgz = '/data/web_static/releases/' + "{}".format(filename.split('.')[0])
-    tmp = "/tmp/" + filename
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
 
     try:
-        put(archive_path, "/tmp/")   # Upload archive to /tmp/ in web-server
-        run("mkdir -p {}/".format(no_tgz))
-        # Uncompress the archive to the folder /data/web_static/releases/
-        # <archive filename without extension> on the web server
-        run("tar -xzf {} -C {}/".format(tmp, no_tgz))
-        run("rm {}".format(tmp))
-        run("mv {}/web_static/* {}/".format(no_tgz, no_tgz))
-        run("rm -rf {}/web_static".format(no_tgz))
-        # Delete the archive from the web server
-        run("rm -rf /data/web_static/current")
-        # Delete the symbolic link /data/web_static/current from the web server
-        run("ln -s {}/ /data/web_static/current".format(no_tgz))
-        # Create a new the symbolic link /data/web_static/current on the
-        # web server, linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension>)
+        put(archive_path, '/tmp')
+        run('mkdir -p {}'.format(dest))
+        run('tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('rm -f /tmp/{}.tgz'.format(name))
+        run('mv {}/web_static/* {}/'.format(dest, dest))
+        run('rm -rf {}/web_static'.format(dest))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(dest))
         return True
     except Exception:
         return False
