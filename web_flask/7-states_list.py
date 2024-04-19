a088b42dfccc79108c281109882b35cb4e1f4659
Palesapp <nnupalesa.pp@gmail.com>
2024-04-19 05:48:29 -0700

    update

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,25 +1,35 @@
 #!/usr/bin/python3
-"""
-starts a Flask web application
-"""
+""" Starts a Flask web application. """
 
 from flask import Flask, render_template
-from models import *
 from models import storage
+from models.state import state
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+# creates an instance of the Flask class and assigns it to the variable app
 app = Flask(__name__)
 
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """display a HTML page with the states listed in alphabetical order"""
-    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
-    return render_template('7-states_list.html', states=states)
-
-
+# Teardown app context to remove the
+# current SQLAlchemy session after each requst
 @app.teardown_appcontext
-def teardown_db(exception):
-    """closes the storage on teardown"""
+def teardown(exception):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+#Define the route for '/states_list'
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Displays an HTML page with a list of all state objects in DBStorage.
+
+    states are sorted by name.
+    """
+    # Fetch all state objects from the DBStorage and sort them by name (A->Z)
+    state = sorted(storage.all(state).value(), key=lambda s: s.name)
+
+    # Render the template and pass the list of states to the template
+    return render_template("7-states_list.html", states=states)
+
+if __name__ == "__main__":
+    # Start the Flask development server
+    # Listen on all available network interface (0.0.0.0) and port 5000
+    app.run(host="0.0.0.0", port=5000)
